010213ab66b37936160fb652fde8168952df194a
nielvid <nielvidservices@gmail.com>
2020-11-30 16:23:48 +0100

    Toggle className in react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+
+import styled from 'styled-components';
+import {useRef, useState} from "react"
+
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const myBox =useRef()
+
+  const [isOpen, setOpen] = useState('false')
+
+
+const Toggle1 = (e)=>{
+ e.target.classList.toggle('close')
 }
 
-export default App;
+const Toggle2 = ()=>{
+ setOpen(!isOpen) 
+}
+
+const Toggle3 = (e)=>{
+myBox.current.classList.toggle('close')  
+}
+
+
+  
+  return (
+    <Wrapper>
+      <h1>Toggling ClassName in React</h1>
+      <p>The first Div Toggles on mouseEnter and onMouseLeave</p>
+      <p>The Second Div Toggles on the first button click</p>
+      <p>The Third toggles on the other button click</p>
+
+
+  <Container>
+   <div className="open"  onMouseEnter={Toggle1} onMouseLeave={Toggle1}>
+    <p style={{color: 'red'}}>Event Target to toggle</p>
+    </div>
+   
+
+    <div className={isOpen ? 'open': 'close'}  >
+    <p>React useState to toggle</p>
+  </div>
+    
+
+    <div className='open' ref={myBox} >
+    <p style={{color: 'blue'}}>I use React ref hook to toggle</p>
+   </div>
+   
+   </Container>
+
+<button onClick= {Toggle2}>Toggle Second Div</button> |  <button onClick= {Toggle3}>Toggle Last Div</button>
+   </Wrapper>
+  );
+
+
+}
+
+
+
+export default App
+
+const Wrapper = styled.div `
+  width: 100%;
+  padding: 25px;
+  `
+
+  const Container = styled.div`
+  width: 100%;
+  padding: 30px;
+  display: flex;
+  justify-content: space-evenly;
+
+
+  .open{
+    font-size:25px;
+    display: block;
+  }
+
+  .close{
+    display: none;
+  }
+  `
+
+
+
+    
